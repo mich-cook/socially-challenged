5ee5561876ecfebfba4cb61e2c3e46593c446fdd
mich <mich-cook@users.noreply.github.com>
2021-01-03 15:46:48 -0800

    adding about page stub

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>Socially Challenged</h1>
+      <h2>Social Fitness Group Challenges</h2>
+      <p>A site for coordinating challenge progress and results.</p>
+    </div>
+  );
+};
+

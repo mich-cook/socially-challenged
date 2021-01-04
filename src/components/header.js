606eb1da1a6329b7517e9d9879f84e47921c5705
mich <mich-cook@users.noreply.github.com>
2021-01-03 21:28:47 -0800

    inline style example in header component

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import styles from "./header.module.css";
 
 export default function Header(props) {
   return (
-    <header className={styles.header}>
+    <header className={styles.header} style={{padding: `0.5rem 1rem` }}>
       <h1>{props.heading}</h1>
       <h2>{props.subheading}</h2>
     </header>

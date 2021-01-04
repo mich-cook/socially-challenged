de98e6e0e09bb11897471a37a445a6f16ed81664
mich <mich-cook@users.noreply.github.com>
2021-01-03 16:09:04 -0800

    adding privacy page stub

---
+++ src/pages/privacy.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "../components/header.js";
+
+export default function Privacy() {
+  return (
+    <div>
+      <Header heading="Socially Challenged" subheading="Social Fitness Group Challenges" />
+      <p>Explain what data we get and how we don't care about the user data other than authentication and authorization needed for site funcitonality.</p>
+    </div>
+  );
+};
+
+

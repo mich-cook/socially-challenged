07760a3285c6fdc0d6f6f96dea54fded9c033f10
mich <mich-cook@users.noreply.github.com>
2021-02-02 22:22:05 -0800

    footer using footer element and requesting data to show that data can still be requested

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "gatsby";
 
-export default function Footer() {
-  return (
-    <ul>
-      <li><Link to="/privacy/">Privacy</Link></li>
-    </ul>
-  );
+export default class Footer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clock: "now"
+    };
+  }
+
+  async componentDidMount() {
+    let newTime = await fetch("http://worldclockapi.com/api/json/utc/now").then();
+    newTime = await newTime.json();
+    this.setState({ "clock": newTime.currentFileTime });
+  }
+
+  render() {
+    return (
+      <footer>
+        <ul>
+          <li><Link to="/privacy/">Privacy</Link></li>
+        </ul>
+        <p>The time is: {this.state.clock}</p>
+      </footer>
+    );
+  }
+
 };

e37a8c0fb922dc473ee2bc3ff78ee4a88a7fbb88
mich <mich-cook@users.noreply.github.com>
2021-02-02 22:26:09 -0800

    adding <main> element around the content main content children to deduplicate it in the rest of the pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Header heading="Socially Challenged" subheading="Social Fitness Group Challenges" />
-      {children}
+      <main>{children}</main>
       <Footer />
     </>
   );

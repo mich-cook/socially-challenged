27f01f0f367f61f7938143c11afef580973ea48a
mich <mich-cook@users.noreply.github.com>
2021-02-05 16:31:37 -0800

    just a tiny bit of content for the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout.js";
 export default function Home() {
   return (
     <Layout>
+      <p>An app that lets users create and participate in group fitness challenges.</p>
     </Layout>
   );
 };

424c3ba0919548274546d2fc3ecf2bc704ee156d
mich <mich-cook@users.noreply.github.com>
2021-02-05 16:12:48 -0800

    adding gatsby style auth using their tutorial (privateRoute, auth, and login are stright from it) to put challenge pages behind auth. TODO: plug in real auth

---
+++ src/pages/challenges.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+import Layout from "../components/layout.js";
+import PrivateRoute from "../components/privateRoute.js";
+import Challenges from "../components/challenges/index.js";
+import CreateChallenge from "../components/challenges/create.js";
+
+export default function ChallengesPage() {
+  return (
+    <Layout>
+      <Router>
+        <PrivateRoute path="/challenges" component={Challenges} />
+        <PrivateRoute path="/challenges/create" component={CreateChallenge} />
+      </Router>
+    </Layout>
+  );
+};

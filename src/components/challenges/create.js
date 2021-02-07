07f55f8eae6f42af51fca69db562de392f4c69e6
mich <mich-cook@users.noreply.github.com>
2021-02-06 16:09:28 -0800

    updating the create challenges form to use controlled components

--- src/components/challenges/create.js
+++ src/components/challenges/create.js
@@ -6,21 +6,42 @@ export default class Create extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
-    
+    this.state = {
+      "title": "",
+      "teams": false,
+      "isPrivate": true
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    const target = e.target;
+    const value = (target.type === "checkbox") ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
     return (
       <Layout>
         <p>Create Your New Social Challenge</p>
-        <label style={{ "display": "block" }}>Name: <input name="title" type="text" length="20" /></label>
-        <label style={{ "display": "block" }}>Teams? <input name="teams" type="checkbox" /></label>
-        <label style={{ "display": "block" }}>Private? <input name="private" type="checkbox" /></label>
-        <div style={{ "border" : "1px solid black", "padding": "10px 20px" }}>
-          <p>Challenge Metrics</p>
-        </div>
-        <button>Create</button>
+        <form onSubmit={this.handleSubmit}>
+          <label style={{ "display": "block" }}>Name: <input name="title" type="text" length="20" value={this.state.title} onChange={this.handleInputChange} /></label>
+          <label style={{ "display": "block" }}>Teams? <input name="teams" type="checkbox" checked={this.state.teams} onChange={this.handleInputChange} /></label>
+          <label style={{ "display": "block" }}>Private? <input name="isPrivate" type="checkbox" checked={this.state.isPrivate} onChange={this.handleInputChange} /></label>
+          <div style={{ "border" : "1px solid black", "padding": "10px 20px" }}>
+            <p>Challenge Metrics</p>
+          </div>
+          <button type="submit">Create</button>
+        </form>
       </Layout>
     );
   }

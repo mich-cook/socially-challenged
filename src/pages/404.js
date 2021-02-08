329fc0f432acdeab4eb77548c56df3b481f5f931
mich <mich-cook@users.noreply.github.com>
2021-02-07 21:29:22 -0800

    adding basic 404 page that we'll improve later so gatsby stops complaining about its absence

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Layout from "../components/layout.js";
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <p>Couldn't find that page.</p>
+    </Layout>
+  );
+};
+

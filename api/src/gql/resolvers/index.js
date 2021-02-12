a3e1f58780be8d4f4ba8aa5dd0f361bc054e3c63
mich <mich-cook@users.noreply.github.com>
2021-02-12 00:28:45 -0800

    connect gql mutation to data store and remove local temp data which is now unused

--- api/src/gql/resolvers/index.js
+++ api/src/gql/resolvers/index.js
@@ -1,10 +1,3 @@
-// until we hook up to real data storage
-let challenges = [
-  { "id": "1", "start": "2020-01-01", "end": "2020-12-31", "cutoff": "2020-01-15", "metrics": "metric placeholder 1" },
-  { "id": "2", "start": "2020-01-01", "end": "2020-01-31", "cutoff": "2020-01-05", "metrics": "metric placeholder 2", "teams": true },
-  { "id": "3", "start": "2020-02-01", "end": "2020-02-15", "cutoff": "2020-02-10", "metrics": "metric placeholder 3", "isPrivate": false }
-];
-
 module.exports = {
   "Query": {
     "challenges": async (parent, args, { models }) => {
@@ -15,16 +8,13 @@ module.exports = {
     }
   },
   "Mutation": {
-    "newChallenge": (parent, args) => {
-      let challenge = {
-        "id": String(challenges.length + 1),
+    "newChallenge": async (parent, args, { models }) => {
+      return await models.Challenge.create({
         "start": args.start,
         "cutoff": args.cutoff,
         "end": args.end,
-        "metrics": args.metrics 
-      };
-      challenges.push(challenge);
-      return challenge;
+        "metrics": args.metrics
+      });
     }
   }
 };

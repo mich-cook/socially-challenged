fb0e522ef57df50974e1783eb639cf58089d5ae0
mich <mich-cook@users.noreply.github.com>
2021-02-14 21:58:58 -0800

    require() cors so we can use it

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { ApolloServer } = require("apollo-server-express");
 

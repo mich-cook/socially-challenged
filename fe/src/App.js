bb79e421420c8d68a2ed8c400b6b2f7e82511980
mich <mich-cook@users.noreply.github.com>
2021-02-21 23:06:57 -0800

    initial commit of fe not using gatsby

---
+++ fe/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>TBD</h1>
+    </React.Fragment>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("react-root"));

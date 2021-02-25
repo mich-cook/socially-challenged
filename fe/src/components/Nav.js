6179fde78f22031b47b7504e891359cf97fcc407
mich <mich-cook@users.noreply.github.com>
2021-02-24 16:14:08 -0800

    a few more basic styled components for a tiny amount of layout

--- fe/src/components/Nav.js
+++ fe/src/components/Nav.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const PageNav = styled.nav`
+  padding: 1em;
+  @media (min-width: 640px) {
+    width: 200px;
+    overflow-y: auto;
+  }
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  line-height: 1.5;
+
+  a {
+    font-size: 1.25rem;
+  }
+`;
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
+    <PageNav>
+      <NavList>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/privacy">Privacy</Link></li>
         <li><Link to="/challenges">Challenges</Link></li>
         <li><Link to="/login">Login</Link></li>
-      </ul>
-    </nav>
+      </NavList>
+    </PageNav>
   );
 };
--- fe/src/components/layout.js
+++ fe/src/components/layout.js
@@ -1,17 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 import Header from "./header.js";
 import Footer from "./footer.js";
 import Nav from "./Nav.js";
 
+const Content = styled.main`
+  @media(min-width: 640px) {
+    display: flex;
+  }
+`;
+
+const Main = styled.main`
+  @media (min-width: 640px) {
+    flex: 1;
+  }
+`;
+
 export default function Layout({ children }) {
   return (
     <>
       <Header heading="Socially Challenged" subheading="Social Fitness Group Challenges" />
-      <div className="content">
+      <Content>
         <Nav />
-        <main>{children}</main>
-      </div>
+        <Main>{children}</Main>
+      </Content>
       <Footer />
     </>
   );

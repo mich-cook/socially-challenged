78989d1595542413c0c997e6e03cb890c56a74a3
mich <mich-cook@users.noreply.github.com>
2021-02-24 22:47:58 -0800

    making apollo client available to app and simple initial implementation on challenges page

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import GlobalStyles from "./components/GlobalStyle.js";
 import Pages from "./pages/index.js";
 
+const uri = process.env.API_URL;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  "connectToDevTools": true
+});
+
 const App = () => {
   return (
-    <React.Fragment>
+    <ApolloProvider client={client}>
       <GlobalStyles />
       <Pages />
-    </React.Fragment>
+    </ApolloProvider>
   );
 };
 
--- fe/src/pages/challenges.js
+++ fe/src/pages/challenges.js
@@ -1,17 +1,47 @@
 import React, { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
 
 import Button from "../components/Button.js";
 
+const GQLGetChallenges = gql`
+  query ChallengeList($cursor: String) {
+    challengeList(cursor: $cursor) {
+      challenges {
+        id
+        createdAt
+        participantCount
+        owner {
+          id
+          displayName
+          username
+        }
+      }
+      cursor
+      continued
+    }
+  }
+`;
+
 export default function Challenges() {
 
+  // this needs to be before the gql loading/error "handlers"
   useEffect(() => {
     document.title = `Socially Challenged | Challenges`;
   });
 
+  const { data, loading, error, fetchMore } = useQuery(GQLGetChallenges);
+
+  if (loading === true) return <p>Loading...</p>;
+  if (error !== undefined) return <p>Error fetching challenges. This is not good.</p>;
+
   return (
     <React.Fragment>
-      <p>List of challenges and ability to create new one</p>
       <Button>Create Challenge</Button>
+      <ul>
+      {data.challengeList.challenges.map((challenge,i) => {
+        return <li key={i}>{challenge.id}</li>;
+      })}
+      </ul>
     </React.Fragment>
   );
 };

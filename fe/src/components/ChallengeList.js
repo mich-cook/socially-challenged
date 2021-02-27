170bcbb705aa3247ba398367cc60e636cde48e36
mich <mich-cook@users.noreply.github.com>
2021-02-26 20:59:29 -0800

    style the challenge list so it doesn't look like a ul so much

--- fe/src/components/ChallengeList.js
+++ fe/src/components/ChallengeList.js
@@ -1,13 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 import Challenge from "./Challenge.js";
 
+const ChallengeList = styled.ul`
+  margin: 0; padding: 0;
+`;
+
 export default ({ challenges }) => {
   return (
-    <ul>
+    <ChallengeList>
       {challenges.map(challenge => {
         return <Challenge challenge={challenge} key={challenge.id} />;
       })}
-    </ul>
+    </ChallengeList>
   );
 };

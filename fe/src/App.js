38797bedf8664d34bd3bea5ab442a97d252d8768
mich <mich-cook@users.noreply.github.com>
2021-02-27 00:22:54 -0800

    set Authorization header to use JWT when it is in LS

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/link-context";
 
 import GlobalStyles from "./components/GlobalStyle.js";
 import Pages from "./pages/index.js";
 
 const uri = process.env.API_URL;
 const cache = new InMemoryCache();
+const httpLink = createHttpLink({ uri });
+const authLink = setContext((X, { headers }) => {
+  return {
+    "headers": {
+      ...headers,
+      "Authorization": localStorage.getItem("token") || ""
+    }
+  };
+});
 
 const client = new ApolloClient({
-  uri,
+  "link": authLink.concat(httpLink),
   cache,
   "connectToDevTools": true
 });

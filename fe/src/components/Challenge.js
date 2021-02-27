e58e4f3ef128ec2abd178f969cbcc8f2419dc138
mich <mich-cook@users.noreply.github.com>
2021-02-26 20:58:48 -0800

    slightly style the items in a challenge list

--- fe/src/components/Challenge.js
+++ fe/src/components/Challenge.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { format, parseISO } from "date-fns";
+import styled from "styled-components";
+
+const ChallengeListItem = styled.li`
+  list-style-type: none;
+  margin: 0; padding: 0;
+  padding-top: 0.5rem;
+  max-width: 640px;
+`;
+
+const ChallengeRow = styled.p`
+  margin: 0; padding: 0;
+`;
 
 export default ({ challenge }) => {
   return (
-    <li><p>{challenge.id} by {challenge.owner.displayName} with {challenge.participantCount} participant(s)</p>
-        <p>Created: {format(parseISO(challenge.createdAt), `MMM do, yyyy`)}</p></li>
+    <ChallengeListItem><ChallengeRow>{challenge.id} by {challenge.owner.displayName} with {challenge.participantCount} participant(s)</ChallengeRow>
+        <ChallengeRow>Created: {format(parseISO(challenge.createdAt), `MMM do, yyyy`)}</ChallengeRow></ChallengeListItem>
   );
 };

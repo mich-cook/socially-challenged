f340d0c0e06829f91749aee3fa0b0e85495fba61
mich <mich-cook@users.noreply.github.com>
2021-02-28 00:10:09 -0800

    get lilo status from apollo client and show appropriate user actions based on that state

--- fe/src/components/header.js
+++ fe/src/components/header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 import styled from "styled-components";
 
 const PageHeader = styled.header`
@@ -15,11 +17,33 @@ const H2 = styled.h2`
   margin: 0;
 `;
 
-export default function Header(props) {
+const UserState = styled.div`
+  p {
+    margin: 0; padding: 0;
+    margin-top: 0.5rem;
+  }
+`;
+
+const GQLLoggedIn = gql`{ lilo @client }`;
+
+export default (props) => {
+
+  const { data } = useQuery(GQLLoggedIn);
+
   return (
     <PageHeader>
       <H1>{props.heading}</H1>
       <H2>{props.subheading}</H2>
+      <UserState>
+        {data.lilo.isLoggedIn ? (
+          <p><Link to="/logout">Log Out</Link></p>
+        ) : (
+          <p>
+            <Link to="/signin">Sign In</Link> |
+            <Link to="/signup">Sign Up</Link>
+          </p>
+        )}
+      </UserState>
     </PageHeader>
   );
 };

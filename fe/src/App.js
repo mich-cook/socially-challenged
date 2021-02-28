5e7657c38fffae4a5e0a0ca573965b86d004bd55
mich <mich-cook@users.noreply.github.com>
2021-02-28 00:03:34 -0800

    use apollo client's local state management to store logged in/out state

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from "@apollo/client";
 
 import GlobalStyles from "./components/GlobalStyle.js";
 import Pages from "./pages/index.js";
 
 const uri = process.env.API_URL;
 const cache = new InMemoryCache();
+
+// TODO: doesn't verify that token is valid and it should
+const lilo = {
+  "isLoggedIn": !!localStorage.getItem("token")
+};
+
+// write this into the cache before init the client
+// .writeData() has been removed and breaks
+cache.writeQuery({
+  "query": gql`{ lilo }`,
+  "data": { lilo }
+});
 
 const client = new ApolloClient({
   uri,

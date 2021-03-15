fc1e5d0db19c00948b1c33b2f309c0b497d02ae1
mich <mich-cook@users.noreply.github.com>
2021-03-14 22:10:32 -0700

    add ChallengeControls to challenge display

--- fe/src/components/Challenge.js
+++ fe/src/components/Challenge.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { format, parseISO } from "date-fns";
 import styled from "styled-components";
+import { useQuery } from "@apollo/client";
 
-import UserList from "./UserList.js"
+import UserList from "./UserList.js";
+import ChallengeControls from "./ChallengeControls.js";
+
+import { GQLLoggedIn } from "../gql/query.js";
 
 const ChallengeListItem = styled.li`
   list-style-type: none;
@@ -17,11 +21,21 @@ const ChallengeRow = styled.p`
 `;
 
 export default ({ challenge }) => {
+
+  const { loading, data, error } = useQuery(GQLLoggedIn);
+  if (loading === true) return <p>Loading data.</p>;
+  if (error !== undefined) return <p>Error loading data.</p>;
+
   return (
     <ChallengeListItem>
       <ChallengeRow><Link to={`challenge/${challenge.id}`}>{challenge.id}</Link> by {challenge.owner.displayName} with {challenge.participantCount} participant(s)</ChallengeRow>
       <ChallengeRow>Created: {format(parseISO(challenge.createdAt), `MMM do, yyyy`)}</ChallengeRow>
       <ChallengeRow>Metrics: {challenge.metrics}</ChallengeRow>
+      {data.lilo.isLoggedIn === true ? (   // TODO: revisit this. obviously not enthusiastic about else
+        <ChallengeRow><ChallengeControls challenge={challenge} /></ChallengeRow>
+      ) : (
+        <ChallengeRow></ChallengeRow>
+      )}
       <UserList users={challenge.participants} />
     </ChallengeListItem>
   );
